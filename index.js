93e8887f3319779c7362591182b09b8a50ab6b06
brunohgv <brunohgv2@gmail.com>
2019-02-28 18:54:09 -0300

    Add first endpoint

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 8600
 
 var app = express()
 
+app.get('/', (req, res) => {
+  res.send('Server Running')
+})
+
 app.listen(PORT, () => {
   console.log(`runnig on port ${PORT}`)
 })

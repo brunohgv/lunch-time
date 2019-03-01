b0fdde6e60b43f1ffdb7aaa199819a584f144a02
Bruno Henrique Gusmão Vasconcelos <brunohgv2@gmail.com>
2019-02-28 22:37:24 -0300

    Branch dev (#3)
    * Add Package.json and index.js
    
    * Add dotenv
    
    * Add first endpoint
    
    * Add procfile to Continuous Integration
    
    * Broadcast and notification

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const express = require('express')
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 require('dotenv').config()
 
 const PORT = process.env.PORT || 8600
 
-var app = express()
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', socket => {
+  socket.on('lunchtime', () => {
+    socket.broadcast.emit('lunchtime')
+  })
+})
+
+http.listen(PORT, () => {
 
 app.get('/', (req, res) => {
   res.send('Server Running')
